8d04f197d0de086ce0420eb04db719eafd0bb087
Francesco Pio Soprano <francesco.soprano@finconsgroup.com>
2024-12-09 14:19:18 +0100

    first commit

---
+++ src/main/java/com/soprano/francesco/BookingAppApplication.java
@@ -0,0 +1,15 @@
+package com.soprano.francesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class BookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppApplication.class, args);
+	}
+
+}
